bdc7ac2e1aa8e6c69a83992f9b6900e6f9ddd580
dr96mabuza <dr96mabuza@gmail.com>
2022-06-16 12:23:31 +0200

    added placeholder text and icons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css";
+import image from "./images/pexels-pixabay-210881.jpg";
+
+const project1 = document.querySelector("#project1");
+const project2 = document.querySelector("#project2");
+const project3 = document.querySelector("#project3");
+const project4 = document.querySelector("#project4");
+const project5 = document.querySelector("#project5");
+const project6 = document.querySelector("#project6");
+
+const myImage = new Image();
+myImage.src = image;
+
+project1.appendChild(myImage);
+project2.appendChild(myImage);
+project3.appendChild(myImage);
+project4.appendChild(myImage);
+project5.appendChild(myImage);
+project6.appendChild(myImage);
