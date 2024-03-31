7931459dddd500d5e274f4674334ffa0756e1c6f
Sifiso <dr96mabuza@gmail.com>
2024-03-31 18:53:03 +0200

    refactor

---
+++ src/form/form.js
@@ -0,0 +1,18 @@
+const getFormData = () => {
+  const firstName = document.querySelector("#first_name").value;
+  const lastName = document.querySelector("#last_name").value;
+  const subject = document.querySelector("#subject").value;
+  const email = document.querySelector("#email").value;
+  const message = document.querySelector("#message_input").value;
+
+  return {
+    firstName, lastName, subject, email, message,
+  };
+};
+
+const handleForm = (event) => {
+  event.preventDefault();
+  return getFormData(event);
+};
+
+export default handleForm;
