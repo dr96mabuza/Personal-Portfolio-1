d0e41f2aae4a7aa8c421ee5bc0bc75ff9f230215
vboxuser <vboxuser@DevEnv.myguest.virtualbox.org>
2025-03-13 16:13:48 +0200

    fixed projects images display

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -3,21 +3,20 @@ import img1 from "./../../asserts/images/Screenshot from 2024-03-11 12-41-12.png
 import img2 from "./../../asserts/images/pankaj-patel-_SgRNwAVNKw-unsplash (1).jpg";
 import img3 from "./../../asserts/images/Screenshot from 2024-03-11 13-02-41.png";
 
+const images = [img1,img2,img3];
 const projects = document.createElement("section");
 projects.setAttribute("id", "projects");
 const header = document.createElement("h2");
 header.textContent = "Projects";
 projects.appendChild(header);
 
-projectsList.forEach((element) => {
+projectsList.forEach((element, index) => {
   const projectContainer = document.createElement("div");
   projectContainer.setAttribute("class", "project");
   const div = document.createElement("div");
   const img = new Image();
   img.setAttribute("alt", element.name);
-  [img1, img2, img3].map((item) => {
-    item === element.src ? (img.src = item) : "";
-  });
+  img.src = images[index];
 
   const h2 = document.createElement("h2");
   h2.setAttribute("class", "projectName");
@@ -31,6 +30,7 @@ projectsList.forEach((element) => {
   projectContainer.appendChild(div);
   projectContainer.appendChild(img);
   projects.appendChild(projectContainer);
+
 });
 
 export default projects;
